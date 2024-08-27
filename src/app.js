4d7f30c3ac07492379be4c9c88c186a2734fad49
mhavliczek <mhavliczek@gmail.com>
2024-08-26 21:20:29 -0400

    cambios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['el', 'La','Los','ellos','su'];
+  let adj = ['Gran', 'Grande','Rojo','Chicos','Gordos'];
+  let noun = ['Comilones', 'Bestias','Starks','Salchichas','Peloscrespos'];
+  let dominios =['com','es','dev','info','io'];
+  
+  for(index1 in pronoun){
+      for(index2 in adj){
+          for(index3 in noun){
+              for(index4 in dominios){
+                  console.log(`${pronoun[index1]}${adj[index2]}${noun[index3]}.${dominios[index3]}`);
+              }
+              
+          }
+      }
+  }
 };
